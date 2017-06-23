bf740edb33ef4f7d3e22458c452af0759047170d
lctsongas <ctsongas94@gmail.com>
2017-06-22 21:51:14 -0500

    ONLY USE THIS! Let me know if you have problems.
    This will work independently on any number of Pis and computers.
    Please let me know if you get any errors!

--- NetworkScripts/MeshNetworkUtil.py
+++ NetworkScripts/MeshNetworkUtil.py
@@ -1,34 +1,72 @@
-from Tkinter import *
-import tkMessageBox
-from PIL import Image, ImageTk
-import socket, threading, sys, traceback, os
-
+import socket, threading, sys
+import traceback, os
 import time, math
+from Queue import Queue
+from MeshUDPPacket import MeshPacket
 
 class MeshNetworkUtil:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	except socket.error:
-	    print 'Failed to create socket'
-	    sys.exit()
+    HOST = ''   # Listen to all 
+    PORT = 7331 # Random port
+    DONE = False
+    isServer = True
+    clientPorts = {'127.0.0.1' : 7331 }
+    
+    # Datagram (udp) socket
+    def __init__(self):
+        """Open socket for listening"""
+        try :
+            self.socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.clientPorts[socket.gethostbyname(socket.gethostname())] = self.PORT
+        except socket.error, msg :
+            print 'Ignore error if this is the 2nd instance'
+            print '[' + str(msg[0]) + '] : ' + msg[1]
+        # Bind socket to local host and port
+        try:
+            self.socketUDP.bind((self.HOST, self.PORT))
+            print 'Socket bind complete'
+        except socket.error , msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            self.isServer = False
+        if self.isServer:
+            self.startListening()
  
-	host = 'localhost';
-	port = 8888;
- 
-	while(1) :
-	    msg = raw_input('Enter message to send : ')
+    #now keep listening with the client
+    def startListening(self):
+        """Create new thread for UDP"""
+        threading.Thread(target=self.listenUDP).start()
+        self.queueEvent = threading.Event()
+        self.queueEvent.clear()
+
+    def listenUDP(self):
+        """Listen for UDP traffic"""
+        while True:
+            # receive data from client (data, addr)
+            d = self.socketUDP.recvfrom(20480)
+            data = d[0]
+            addr = d[1]
+            if not data: 
+                break
      
-	    try :
-		#Set the whole string
-		s.sendto(msg, (host, port))
-         
-		# receive data from client (data, addr)
-		d = s.recvfrom(1024)
-		reply = d[0]
-		addr = d[1]
-         
-		print 'Server reply : ' + reply
+            #reply = 'OK...' + data
      
-	    except socket.error, msg:
-		print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-		sys.exit()
+            #self.socketUDP.sendto(reply , addr)
+            if data == 'STOP':
+                self.closeSocket()
+            print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+            self.clientPorts[addr[0]] = addr[1]
+
+    def closeSocket(self):
+        self.DONE = True
+        self.socketUDP.close()
+
+    def sendData(self, data, host):
+        try :
+            #Set the whole string
+            self.socketUDP.sendto(data, (host, self.clientPorts[host]))
+            self.startListening()
+            #d = self.socketUDP.recvfrom(20480)
+            #reply = d[0]
+            #addr = d[1]
+        except socket.error, msg:
+            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            self.closeSocket()
