2087ab31a99548ba5a2dda1d4a2d9321f796e279
lctsongas <ctsongas94@gmail.com>
2017-07-17 03:52:34 +0000

    Add files via upload

---
+++ Network/hsmm-templates/AppController.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Application level Controller
+ *
+ * This file is application-wide controller file. You can put all
+ * application-wide controller-related methods here.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+App::uses('Controller', 'Controller');
+
+class AppController extends Controller {
+
+	protected $olsrd_fixed_location_file = '/var/run/latlong-input-olsrd';
+
+	public $components = array('Session',
+		'Auth' => array('loginRedirect' => array('controller' => 'status',
+			'action' => 'index'),
+			'logoutRedirect' => array('controller' => 'status',
+				'action' => 'index')));
+
+	protected function load_node_attributes() {
+		$this->loadModel('NetworkSetting');
+		$settings = $this->NetworkSetting->findById(1);
+		$this->set('node_name', $settings['NetworkSetting']['node_name']);
+		$this->set('node_wifi_ip_address', $settings['NetworkSetting']['wifi_ip_address']);
+	}
+
+	protected function get_location() {
+		$this->loadModel('LocationSetting');
+		return $this->LocationSetting->findById(1);
+	}
+
+	protected function get_network_settings() {
+		$this->loadModel('NetworkSetting');
+		return $this->NetworkSetting->findById(1);
+	}
+
+	protected function get_network_services() {
+		$this->loadModel('NetworkService');
+		return $this->NetworkService->find('all');
+	}
+
+	protected function render_ntp_config($network_setting, $location) {
+		if (0 == strcasecmp($location['LocationSetting']['location_source'], 'gps')) {
+			$gpsd_time_server_info =
+			"
+server 127.127.28.0
+fudge 127.127.28.0 time1 0.420 refid GPS
+
+server 127.127.28.1 prefer
+fudge 127.127.28.1 refid GPS1";
+		} else {
+			$gpsd_time_server_info = "";
+		}
+
+		$ntp_config = file_get_contents(WWW_ROOT . "/files/ntp.conf.template");
+		$ntp_config_output = str_replace(array('{gpsd_time_server_info}',
+			'{ntp_server}'),
+			array($gpsd_time_server_info,
+				$network_setting['NetworkSetting']['ntp_server']),
+			$ntp_config);
+		file_put_contents('/etc/ntp.conf', $ntp_config_output);
+	}
+
+	protected function render_rclocal_config($network_setting, $network_services) {
+		$rclocal_conf = file_get_contents(WWW_ROOT . "/files/rc.local.template");
+
+		$iptables_service_routing = "";
+		if (0 == strcmp($network_setting['NetworkSetting']['wired_interface_mode'], 'WAN')) {
+			if ($network_setting['NetworkSetting']['wan_mesh_gateway'] == TRUE) {
+				$iptables_gateway_commands =
+				"
+iptables -t nat -A POSTROUTING -o " . $network_setting['NetworkSetting']['wired_adapter_name'] . " -j MASQUERADE
+iptables -A FORWARD -i " . $network_setting['NetworkSetting']['wired_adapter_name'] . " -o " . $network_setting['NetworkSetting']['wifi_adapter_name'] . " -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i " . $network_setting['NetworkSetting']['wifi_adapter_name'] . " -o " . $network_setting['NetworkSetting']['wired_adapter_name'] . " -j ACCEPT
+";
+			} else {
+				$iptables_gateway_commands = "";
+			}
+		} else {
+			$iptables_gateway_commands =
+			"
+# Flush the tables
+iptables -F INPUT
+iptables -F OUTPUT
+iptables -F FORWARD
+
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+iptables -t nat -P OUTPUT ACCEPT
+
+# Allow forwarding packets:
+iptables -A FORWARD -i " . $network_setting['NetworkSetting']['wired_adapter_name'] . " -j ACCEPT
+iptables -A FORWARD -i " . $network_setting['NetworkSetting']['wifi_adapter_name'] . " -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Packet masquerading
+iptables -t nat -A POSTROUTING -o " . $network_setting['NetworkSetting']['wifi_adapter_name'] . " -j MASQUERADE ";
+
+			if ($network_services != NULL && sizeof($network_services) > 0) {
+				foreach ($network_services as $service) {
+					$iptables_service_routing .=
+					"
+iptables -t nat -A PREROUTING -i " . $network_setting['NetworkSetting']['wifi_adapter_name'] . " -p " . $service['NetworkService']['protocol'] . " --dport " . $service['NetworkService']['port'] . " -j DNAT --to-destination " . $service['NetworkService']['host'] . ":" . $service['NetworkService']['local_port'] . "
+iptables -t nat -A POSTROUTING -p " . $service['NetworkService']['protocol'] . " --dport " . $service['NetworkService']['port'] . " -j MASQUERADE\n";
+				}
+			}
+		}
+
+		$rclocal_conf_output = str_replace(array('{wifi_adapter_name}',
+			'{iptables_gateway_commands}',
+			'{iptables_service_routing}'),
+			array($network_setting['NetworkSetting']['wifi_adapter_name'],
+				$iptables_gateway_commands, $iptables_service_routing),
+			$rclocal_conf);
+
+		file_put_contents('/etc/rc.local', $rclocal_conf_output);
+	}
+
+	protected function render_olsrd_config($network_setting, $network_services, $location) {
+		$olsrd_conf = file_get_contents(WWW_ROOT . "/files/olsrd/olsrd.conf.template");
+		$olsrd_secure_block = null;
+
+		if ($network_setting['NetworkSetting']['mesh_olsrd_secure'] == TRUE) {
+			$olsrd_secure_block =
+			"LoadPlugin \"olsrd_secure.so.0.6\"
+{
+    PlParam     \"Keyfile\"   \"/etc/olsrd/olsrd.key\"
+}";
+
+		} else {
+			$olsrd_secure_block = "";
+		}
+
+		if ((0 == strcmp($network_setting['NetworkSetting']['wired_interface_mode'], 'WAN')) &&
+			($network_setting['NetworkSetting']['wan_mesh_gateway'] == TRUE)) {
+			$olsrd_network_services = "";
+			if ($network_setting['NetworkSetting']['wan_fixed_connection'] == FALSE) {
+				$olsrd_gateway =
+				"
+LoadPlugin \"olsrd_dyn_gw.so.0.5\"
+{
+    # The plugin check interval can be set here in milliseconds.
+    # The default is 1000 ms (1 second).
+    PlParam     \"CheckInterval\"  \"5000\"
+
+    # The ping check interval in case there is any pinged host specified.
+    # The default is 5 seconds.
+    PlParam     \"PingInterval\"   \"40\"
+
+    # If one or more IPv4 addresses are given, do a ping on these in
+    # descending order to validate that there is not only an entry in
+    # routing table, but also a real network connection. If any of
+    # these addresses could be pinged successfully, the test was
+    # succesful, i.e. if the ping on the 1st address was successful,the
+    # 2nd won't be pinged.
+    #
+    # The Ping list applies to the group of HNAs specified above or to the
+    # default internet gateway when no HNA is specified.
+    #
+    # Running the plugin without parameters acts as the 'old' dyn_gw_plain.
+
+    #   The following ping entries for the internet gateway
+    PlParam \"Ping\"   \"" . $network_setting['NetworkSetting']['wan_dns1'] . "\"
+    PlParam \"Ping\"   \"" . $network_setting['NetworkSetting']['wan_dns2'] . "\"
+}";
+			} else {
+				$olsrd_gateway =
+				"
+LoadPlugin \"olsrd_dyn_gw_plain.so.0.4\"
+{
+}
+";
+			}
+		} else {
+			$olsrd_gateway = "";
+			$olsrd_network_services = "";
+		}
+
+		if ($network_services != NULL && sizeof($network_services) > 0) {
+			foreach ($network_services as $service) {
+				$olsrd_network_services .= "
+    PlParam \"service\" \"" . $service['NetworkService']['service_protocol_name'] . "://" . $network_setting['NetworkSetting']['node_name'] . ":" . $service['NetworkService']['port'] . "/" . $service['NetworkService']['path'] . "|" . $service['NetworkService']['protocol'] . "|" . $service['NetworkService']['name'] . "\"
+";
+			}
+		}
+
+		$transmit_location_option = null;
+		if ($location['LocationSetting']['transmit_location_enabled'] == TRUE) {
+			if (0 == strcmp($location['LocationSetting']['location_source'], 'fixed')) {
+				$transmit_location_option =
+				"
+    PlParam \"lat\" \"" . $location['LocationSetting']['lat'] . "\"
+    PlParam \"lon\" \"" . $location['LocationSetting']['lon'] . "\"
+";
+			} else {
+				$transmit_location_option = "    PlParam \"latlon-infile\" \"" . $this->olsrd_fixed_location_file . "\"";
+			}
+		} else {
+			$transmit_location_option = "";
+		}
+
+		$olsrd_conf_output = str_replace(array('{latlon_infile}',
+			'{wifi_ip_address}',
+			'{wifi_adapter_name}',
+			'{node_name}',
+			'{olsrd_dynamic_gateway_block}',
+			'{olsrd_secure_block}',
+			'{olsrd_network_services}'),
+			array($transmit_location_option,
+				$network_setting['NetworkSetting']['wifi_ip_address'],
+				$network_setting['NetworkSetting']['wifi_adapter_name'],
+				$network_setting['NetworkSetting']['node_name'],
+				$olsrd_secure_block,
+				$olsrd_gateway,
+				$olsrd_network_services),
+			$olsrd_conf);
+
+		file_put_contents('/etc/olsrd/olsrd.conf', $olsrd_conf_output);
+
+		$olsrd_key = null;
+		if ($network_setting['NetworkSetting']['mesh_olsrd_secure_key'] != null) {
+			$olsrd_key = $network_setting['NetworkSetting']['mesh_olsrd_secure_key'];
+		} else {
+			$olsrd_key = "";
+		}
+
+		file_put_contents('/etc/olsrd/olsrd.key', $olsrd_key);
+	}
+}
+?>
---
+++ Network/hsmm-templates/NetworkSettingsController.php
@@ -0,0 +1,165 @@
+<?php
+class NetworkSettingsController extends AppController {
+	public $helpers = array('Html', 'Session');
+	public $components = array('Flash', 'RequestHandler', 'Session');
+
+	public function edit($id = null) {
+		$network_setting = $this->NetworkSetting->findById($id);
+		$mac_file = '/sys/class/net/eth0/address';
+		if (file_exists($mac_file)) {
+                                        $mac_address = explode(':', file_get_contents($mac_file));
+                                        $network_setting['NetworkSetting']['wifi_ip_address'] =
+                                        '10.' .
+                                        hexdec($mac_address[3]) . '.' .
+                                        hexdec($mac_address[4]) . '.' .
+                                        hexdec($mac_address[5]);
+					$mac_eth_file = '/sys/class/net/eth0/address';
+					$mac_eth_addr = explode(':', file_get_contents($mac_eth_file));
+					$network_setting['NetworkSetting']['node_name'] = 
+					'RPi_Node' . hexdec($mac_eth_addr[5]);
+					$network_setting['NetworkSetting']['lan_ip_address'] = 
+					'192.168.' . hexdec($mac_address[5]) . '.1';
+                }
+		if (!$network_setting) {
+			throw new NotFoundException(__('Invalid setting'));
+		}
+
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->NetworkSetting->save($this->request->data)) {
+				
+
+				$latest_network_setting = $this->get_network_settings();
+				$network_services = $this->get_network_services();
+				$location = $this->get_location();
+
+				$this->render_olsrd_config($latest_network_setting, $network_services, $location);
+				$this->render_rclocal_config($latest_network_setting, $network_services);
+				$this->render_network_interfaces_config($latest_network_setting);
+				$this->render_dnsmasq_config($latest_network_setting);
+				$this->render_hostname_config($latest_network_setting);
+				$this->render_dhclient_config($latest_network_setting);
+				$this->render_resolv_config($latest_network_setting);
+				$this->render_hosts_config($latest_network_setting);
+				$this->render_ntp_config($latest_network_setting, $location);
+
+				$this->Flash->reboot(__('Your settings have been saved and will take effect on the next reboot.'));
+			} else {
+				$this->Flash->error(__('Unable to update your settings, please review any validation errors.'));
+			}
+		} else {
+			// perform some checks in the case of an HTTP GET
+			if ($network_setting['NetworkSetting']['wifi_ip_address'] == NULL) {
+				// if no WIFI IP is set, then use one derived from the adapter MAC address
+				$mac_file = '/sys/class/net/' . $network_setting['NetworkSetting']['wifi_adapter_name'] . '/address';
+				if (file_exists($mac_file)) {
+					$mac_address = explode(':', file_get_contents($mac_file));
+					$network_setting['NetworkSetting']['wifi_ip_address'] =
+					'10.' .
+					hexdec($mac_address[3]) . '.' .
+					hexdec($mac_address[4]) . '.' .
+					hexdec($mac_address[5]);
+				}
+			}
+		}
+
+		if (!$this->request->data) {
+			$this->request->data = $network_setting;
+		}
+
+		$this->set('wired_interface_mode', $network_setting['NetworkSetting']['wired_interface_mode']);
+	}
+
+	private function render_resolv_config($network_setting) {
+		// update the resolv.conf file if the wired interface is in LAN mode so that DNS lookup hit the dnsmasq cache
+		// it's possible that the node was previously running with the wired interface in WAN mode and there are left-over
+		// DNS server entries in the resolv.conf file.  Wipe them out.
+		if (0 == strcmp($network_setting['NetworkSetting']['wired_interface_mode'], 'LAN')) {
+			file_put_contents('/etc/resolv.conf', file_get_contents(WWW_ROOT . "/files/resolv.conf.template"));
+		}
+	}
+
+	private function render_hosts_config($network_setting) {
+		$hosts_conf = file_get_contents(WWW_ROOT . "/files/hosts.template");
+		$hosts_conf_output = str_replace(array('{node_name}'), array($network_setting['NetworkSetting']['node_name']), $hosts_conf);
+
+		file_put_contents('/etc/hosts', $hosts_conf_output);
+	}
+
+	private function render_hostname_config($network_setting) {
+		$node_name = $network_setting['NetworkSetting']['node_name'];
+		file_put_contents('/etc/hostname', $node_name);
+	}
+
+	private function render_network_interfaces_config($network_setting) {
+		$interfaces_conf = file_get_contents(WWW_ROOT . "/files/network_interfaces/interfaces.template");
+		$wired_mode_block = null;
+		if (0 == strcmp($network_setting['NetworkSetting']['wired_interface_mode'], 'LAN')) {
+			$wired_mode_block =
+			"static
+    address " . $network_setting['NetworkSetting']['lan_ip_address'] . "
+    netmask " . $network_setting['NetworkSetting']['lan_netmask'] . "
+";
+
+		} else {
+			$wired_mode_block = "dhcp";
+		}
+		$mac_file = '/sys/class/net/' . $network_setting['NetworkSetting']['wifi_adapter_name'] . '/address';
+		if (file_exists($mac_file)) {
+                                        $mac_address = explode(':', file_get_contents($mac_file));
+                                        $network_setting['NetworkSetting']['wifi_ip_address'] =
+                                        '10.' .
+                                        hexdec($mac_address[3]) . '.' .
+                                        hexdec($mac_address[4]) . '.' .
+                                        hexdec($mac_address[5]);
+                }
+		$interfaces_conf_output =
+		str_replace(
+			array('{wired_adapter_name}', '{wifi_protocol}', '{wifi_ip_address}', '{wifi_netmask}', '{wifi_mode}', '{wifi_channel}', '{wifi_ssid}', '{wired_mode}', '{wifi_adapter_name}',  '{lan_ip_address}', '{lan_netmask}'),
+			array(
+				$network_setting['NetworkSetting']['wired_adapter_name'],
+				strtolower($network_setting['NetworkSetting']['wifi_protocol']),
+				$network_setting['NetworkSetting']['wifi_ip_address'],
+				$network_setting['NetworkSetting']['wifi_netmask'],
+				strtolower($network_setting['NetworkSetting']['wifi_mode']),
+				$network_setting['NetworkSetting']['wifi_channel'],
+				$network_setting['NetworkSetting']['wifi_ssid'],
+				$wired_mode_block,
+				$network_setting['NetworkSetting']['wifi_adapter_name'],
+				$network_setting['NetworkSetting']['lan_ip_address'],
+				$network_setting['NetworkSetting']['lan_netmask']
+			),
+			$interfaces_conf);
+
+		file_put_contents('/etc/network/interfaces', $interfaces_conf_output);
+	}
+
+	private function render_dnsmasq_config($network_setting) {
+		$dnsmasq_conf = file_get_contents(WWW_ROOT . "/files/dnsmasq/hsmm-pi.conf.template");
+
+		$dhcp_interface = null;
+		if ($network_setting['NetworkSetting']['lan_dhcp_server'] == TRUE) {
+			$dhcp_interface = "interface=" . $network_setting['NetworkSetting']['wired_adapter_name'];
+		} else {
+			$dhcp_interface = "
+no-dhcp-interface=" . $network_setting['NetworkSetting']['wired_adapter_name'] . "
+no-dhcp-interface=" . $network_setting['NetworkSetting']['wifi_adapter_name'];
+		}
+
+		$ip_parts = split("\.", $network_setting['NetworkSetting']['lan_ip_address']);
+		$lan_dhcp_start = $ip_parts[0] . '.' . $ip_parts[1] . '.' . $ip_parts[2] . '.' . $network_setting['NetworkSetting']['lan_dhcp_start'];
+		$lan_dhcp_end = $ip_parts[0] . '.' . $ip_parts[1] . '.' . $ip_parts[2] . '.' . $network_setting['NetworkSetting']['lan_dhcp_end'];
+
+		$dnsmasq_conf_output = str_replace(array('{node_name}', '{interfaces}', '{lan_ip_address}', '{lan_dhcp_start}', '{lan_dhcp_end}', '{lan_netmask}', '{wan_dns1}', '{wan_dns2}'), array($network_setting['NetworkSetting']['node_name'], $dhcp_interface, $network_setting['NetworkSetting']['lan_ip_address'], $lan_dhcp_start, $lan_dhcp_end, $network_setting['NetworkSetting']['lan_netmask'], $network_setting['NetworkSetting']['wan_dns1'], $network_setting['NetworkSetting']['wan_dns2']), $dnsmasq_conf);
+
+		file_put_contents('/etc/dnsmasq.d/hsmm-pi.conf', $dnsmasq_conf_output);
+	}
+
+	private function render_dhclient_config($network_setting) {
+		$dhclient_conf = file_get_contents(WWW_ROOT . "/files/dhclient.conf.template");
+		$dhclient_conf_output = str_replace('{wired_adapter_name}', $network_setting['NetworkSetting']['wired_adapter_name'], $dhclient_conf);
+		file_put_contents('/etc/dhcp/dhclient.conf', $dhclient_conf_output);
+	}
+
+}
+
+?>
