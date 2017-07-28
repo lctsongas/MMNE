40fe70250cd8406c763f9314c66810c09b1332eb
lctsongas <ctsongas94@gmail.com>
2017-07-28 08:09:26 +0000

    Running on Node 169

--- Tests/BatteryLoadTest.py
+++ Tests/BatteryLoadTest.py
@@ -1,8 +1,7 @@
-import datetime
-from time import time
+import datetime, time
 import sys
 sys.path.append("/home/pi/MMNE/Movement Control System")
-from classes import *
+import classes
 from multiprocessing import Pool, Queue
 from multiprocessing import cpu_count
         
@@ -11,21 +10,23 @@ def dummy(x):
         x*x    
 
 if __name__ == '__main__':
-    #processes = cpu_count()
+    processes = cpu_count()
     
-    #pool = Pool(processes)
-    #pool.map_async(dummy, range(processes))
-    motor = Motor_Operations()
+    pool = Pool(processes)
+    pool.map_async(dummy, range(processes))
+    motor = classes.Motor_Operations()
+    motor.allstop()
     motor.setspeed(250,250,250,250)
     motor.forward()
     ctime = datetime.datetime.now()
     x = 2
-    #with open('batterylog.txt' , 'a') as log:
-        #log.write('Start ' + str(ctime) + '\n')
-        #print 'started'
+    with open('batterylog.txt' , 'a') as log:
+        log.write('Start ' + str(ctime) + '\n')
+        print 'started'
     while True:
-        #with open('batterylog.txt' , 'a') as log:
-            #log.write(str(ctime) + '\n')
+        with open('batterylog.txt' , 'a') as log:
+            ctime = datetime.datetime.now()
+            log.write(str(ctime) + '\n')
         print 'recorded'
         x=x*x
         time.sleep(1)
